b76cb5af26846e92899f8162a1b5b73155b9a10c
devios1337 <lmurray@vivonet.com>
2016-12-27 23:22:36 -0500

    Initial commit

---
+++ Gravl.swift
@@ -0,0 +1,406 @@
+//
+//  Gravl.swift
+//  Gravl
+//
+//  Created by Logan Murray on 2016-12-16.
+//  Copyright © 2016 Logan Murray. All rights reserved.
+//
+
+import Foundation
+
+public class Gravl {
+
+	public class Node {
+		let value: String // the name of a node is its value
+		let attributes: [String: Node]
+		let childNodes: [Node]
+		
+		private static let reservedCharacterSet = CharacterSet(charactersIn: Parser.reservedChars + Parser.whitespaceChars)
+		
+		fileprivate init(value: String, attributes: [String: Node], childNodes: [Node]) {
+			self.value = value
+			self.attributes = attributes
+			self.childNodes = childNodes
+		}
+		
+		public func serialize(options: SerializationOptions = SerializationOptions()) -> String {
+			var result = ""
+			var quote = Node.quoteForSymbol(value)
+			let textNode = self is TextNode
+			let escapedValue = value.replacingOccurrences(of: "\\", with: "\\\\").replacingOccurrences(of: "\"", with: "\\\"")
+			result += "\(textNode ? "" : "[")\(quote)\(escapedValue)\(quote)"
+			var complexAttributes = [String]() // record the keys for attributes that have node values
+			
+			for attribute in attributes.keys.sorted() {
+				if options.complexAttributesOnBottom && !(attributes[attribute] is TextNode) {
+					complexAttributes.append(attribute)
+					continue
+				}
+				let separator = options.inlineAttributes ? " " : "\n\(options.indentation)"
+				quote = Node.quoteForSymbol(attribute)
+				result += "\(separator)\(quote)\(attribute)\(quote)"
+				let attrValue = attributes[attribute]!.serialize(options: options)
+				result += "\(options.beforeEquals)=\(options.afterEquals)\(attrValue)"
+			}
+			
+			for attribute in complexAttributes.sorted() {
+				let separator = "\n\(options.indentation)"
+				quote = Node.quoteForSymbol(attribute)
+				result += "\(separator)\(quote)\(attribute)\(quote)\(options.beforeEquals)="
+				let valueLines = attributes[attribute]!.serialize(options: options)
+				for valueLine in valueLines.components(separatedBy: "\n") {
+					result += "\n\(options.indentation)\(options.indentation)\(valueLine)"
+				}
+			}
+			
+			if !(attributes.isEmpty || options.inlineAttributes) || !complexAttributes.isEmpty && !childNodes.isEmpty {
+				result += options.afterAttributes
+			}
+			
+			for childNode in childNodes {
+				let childLines = childNode.serialize(options: options).components(separatedBy: "\n")
+				for childLine in childLines {
+					result += "\n\(options.indentation)\(childLine)"
+				}
+			}
+			
+			let multiLine = !(attributes.isEmpty || options.inlineAttributes) || !childNodes.isEmpty
+			result += "\(multiLine ? "\n" : "")\(textNode ? "" : "]")"
+			
+			return result
+		}
+		
+		private static func quoteForSymbol(_ symbol: String) -> String {
+			return symbol.rangeOfCharacter(from: reservedCharacterSet) != nil ? "\"" : ""
+		}
+	}
+	
+	// this is an empty subclass to represent implicit (symbolic) nodes;
+	// because it inherits from Node you don't actually have to care unless you need to
+	public class TextNode: Node {
+		fileprivate init(value: String) {
+			super.init(value: value, attributes: [String: Node](), childNodes: [Node]())
+		}
+	}
+	
+	public struct SerializationOptions {
+		public var indentation: String = "  "
+		public var inlineAttributes = true
+		public var afterAttributes = "\n" // put a blank line between attributes and child nodes
+		public var beforeEquals = ""
+		public var afterEquals = ""
+		public var complexAttributesOnBottom = true // sorts complex attributes to the bottom for readability
+	}
+	
+	public class Parser {
+		public static let reservedChars = "[]\"=#"
+		public static let whitespaceChars = " \t\n\r"
+		
+		private let name: String
+		private var buffer: String
+		private var index: String.Index
+		private var glyphIndex: String.Index? = nil // the index of the next glyph, or nil if unknown
+		fileprivate var line = 1
+		fileprivate var col = 0
+		
+		public var document: Node?
+		public var error: ParserError?
+		
+		public init(name: String? = nil) {
+			self.name = name ?? "Document"
+			self.buffer = ""
+			self.index = buffer.startIndex
+		}
+
+		public func parse(string: String) -> Node? {
+			buffer = string
+			index = buffer.startIndex
+			glyphIndex = nil
+			line = 1
+			col = 0
+			document = nil
+			error = nil
+			
+			do {
+				document = try recordNodeBody(name: name)
+				
+				if let char = peekGlyph() {
+					throw ParserError(self, fault: .unexpectedChar(char: char, reason: "Extraneous character found in document. Ensure all brackets and quotes are balanced."))
+				}
+			} catch let error as ParserError {
+				self.error = error
+				print("\(error.errorDescription)")
+				return nil
+			} catch {
+				// this can never happen, but swift complains without it *shrug*
+			}
+			
+			return document
+		}
+		
+		// MARK: Record States
+		
+		private func recordNode() throws -> Node {
+			var char = try readGlyph()
+			assert(char == "[")
+			
+			let name = try recordSymbol()
+			
+			if name == "" {
+				throw ParserError(self, fault: .unexpectedChar(char: try readChar(), reason: "Nodes must be named."));
+			}
+			
+			let node = try recordNodeBody(name: name)
+			
+			char = try readGlyph()
+			if char != "]" { // is this actually possible?
+				throw ParserError(self, fault: .unexpectedChar(char: char, reason: "Attributes must be defined before child nodes."))
+			}
+			
+			return node
+		}
+		
+		private func recordNodeBody(name: String) throws -> Node {
+			var attributes = [String: Node]()
+			var childNodes = [Node]()
+			
+			// look for any attributes
+			while true {
+				let firstSymbol = try recordSymbol()
+				
+				if firstSymbol == "" {
+					break // no more attributes
+				}
+				
+				if peekGlyph() == "=" { // this is an attribute
+					_ = try readGlyph() // absorb the =
+					
+					if attributes[firstSymbol] != nil {
+						throw ParserError(self, fault: .duplicateAttribute(attribute: firstSymbol, node: name))
+					}
+					
+					if peekGlyph() == "[" {
+						let value = try recordNode()
+						
+						attributes[firstSymbol] = value
+					} else {
+						let secondSymbol = try recordSymbol()
+						
+						if secondSymbol == "" {
+							throw ParserError(self, fault: .unexpectedChar(char: try readChar(), reason: "Attributes must have values."))
+						}
+						
+						attributes[firstSymbol] = TextNode(value: secondSymbol)
+					}
+				} else {
+					childNodes.append(TextNode(value: firstSymbol)) // add the symbol as a TextNode and break
+					break // as soon as we record a child node we are no longer in attribute record mode
+				}
+			}
+			
+			// add remaining nodes as children
+			while peekGlyph() != nil && peekGlyph() != "]" { // we also need to consider eof now
+				var node: Node
+				
+				if peekGlyph() == "=" {
+					throw ParserError(self, fault: .unexpectedChar(char: try readGlyph(), reason: "Attributes must be defined before child nodes."))
+				}
+				
+				if peekGlyph() == "[" {
+					node = try recordNode()
+				} else {
+					let value = try recordSymbol()
+					assert(value != "")
+					
+					node = TextNode(value: value)
+				}
+				
+				childNodes.append(node)
+			}
+			
+			return Node(value: name, attributes: attributes, childNodes: childNodes)
+		}
+		
+		private func recordSymbol() throws -> String {
+//			if peekGlyph() == nil {
+//				throw ParserError(self, fault: .unexpectedEOF)
+//			}
+			if peekGlyph() == "\"" {
+				return try recordString()
+			}
+			
+			var symbol = ""
+			
+			while glyphIndex != nil && index != glyphIndex! {
+				_ = try readChar()
+			}
+			
+			while peekChar() != nil && !isReservedChar(peekChar()!) && !isWhitespace(peekChar()!) {
+				symbol.append(try readChar())
+			}
+			
+			return symbol
+		}
+		
+		private func recordString() throws -> String {
+			var char = try readGlyph()
+			assert(char == "\"")
+			
+			var string = ""
+			while peekChar() != nil && peekChar() != "\"" {
+				if peekChar() == "\\" {
+					string.append(try recordEscapedChar())
+				} else {
+					string.append(try readChar())
+				}
+			}
+			
+			char = try readGlyph()
+			assert(char == "\"")
+			
+			return string
+		}
+		
+		private func recordEscapedChar() throws -> Character {
+			var char = try readChar() // absorb \
+			assert(char == "\\")
+
+			char = try readChar()
+			if char != "\\" && char != "\"" {
+				throw ParserError(self, fault: .unexpectedChar(char: char, reason: "A backslash must be followed by either \" or \\."))
+			}
+			
+			return char
+		}
+		
+		// MARK: Helper Methods
+		
+		private func peekChar() -> Character? {
+			if index == buffer.endIndex {
+//				throw ParserError(self, fault: .unexpectedEOF)
+				return nil
+			}
+			
+			return buffer[index]
+		}
+		
+		private func readChar() throws -> Character {
+			if glyphIndex == index { // if we've reached glyphIndex, reset it because it's no longer pointing ahead
+				glyphIndex = nil
+			}
+
+			guard let char = peekChar() else {
+				throw ParserError(self, fault: .unexpectedEOF)
+			}
+			
+			if char == "\n" {
+				line += 1
+				col = 0
+			} else {
+				col += 1
+			}
+			
+			index = buffer.index(after: index) // swift 3, everyone *sigh*
+			
+			return char
+		}
+		
+		// a glyph is any non-whitespace character
+		// comments are also handled (skipped over) at this level, making them effectively equivalent to whitespace
+		private func peekGlyph() -> Character? {
+			if let glyphIndex = glyphIndex { // if glyphIndex is set, it means we've already located the next glyph
+				return buffer[glyphIndex]
+			}
+			
+			//var char = peekChar()
+			var insideComment = false
+			glyphIndex = index
+			
+			while true {
+				if glyphIndex == buffer.endIndex {
+					glyphIndex = nil
+					// no glyph found before the eof
+					return nil
+//					throw ParserError(self, fault: .unexpectedEOF)
+				}
+				
+				// handle # comments
+				if !insideComment && buffer[glyphIndex!] == "#" {
+					insideComment = true
+				} else if insideComment && buffer[glyphIndex!] == "\n" {
+					insideComment = false
+				}
+				
+				if !insideComment && !isWhitespace(buffer[glyphIndex!]) {
+					break
+				}
+				
+				glyphIndex = buffer.index(after: glyphIndex!)
+			}
+			
+			return buffer[glyphIndex!]
+		}
+		
+		private func readGlyph() throws -> Character {
+			guard let glyph = peekGlyph() else { // ensures glyphIndex is set or nil if eof
+				throw ParserError(self, fault: .unexpectedEOF)
+			}
+			
+			while glyphIndex != nil {
+				_ = try readChar() // we need to do this so the line/col counters are accurate
+			}
+			
+			return glyph
+		}
+		
+		private func isWhitespace(_ char: Character) -> Bool {
+//			return char == " " || char == "\t" || char == "\n" || char == "\r"
+			return Parser.whitespaceChars.characters.contains(char)
+		}
+		
+		// any of: [ ] = " #
+		private func isReservedChar(_ char: Character) -> Bool {
+			return Parser.reservedChars.characters.contains(char)
+		}
+	}
+	
+	public struct ParserError: Error {
+		fileprivate enum Fault {
+			case unexpectedChar(char: Character, reason: String)
+			case duplicateAttribute(attribute: String, node: String)
+			case unexpectedEOF
+		}
+		
+		fileprivate let fault: Fault
+		
+		public let line: Int
+		public let col: Int
+		
+		fileprivate init(_ parser: Parser, fault: Fault) {
+			self.fault = fault
+			self.line = parser.line
+			self.col = parser.col
+		}
+		
+		public var message: String {
+			get {
+				switch fault {
+					case .unexpectedChar(let char, let reason):
+						return "Unexpected character: \"\(char)\". \(reason)"
+					
+					case .duplicateAttribute(let attribute, let node):
+						return "Duplicate attribute. The attribute \"\(attribute)\" has already been defined for the node [\(node)]."
+					
+					case .unexpectedEOF:
+						return "Unexpected end of file. Ensure all brackets and quotes are balanced."
+				}
+			}
+		}
+		
+		public var errorDescription: String {
+			get {
+				return "\(message) (Line: \(line), Col: \(col))"
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,158 @@
 # Gravl
-A modern minimalist structured data language in the spirit of XML and JSON
+A modern structured data language in the spirit of XML and JSON
+
+## What is Gravl?
+Gravl is a very simple but surprisingly capable structured object representation language. It is designed to be human-enterable and human-readable and avoids many of the limitations of XML and JSON.
+
+Here is what Gravl looks like:
+
+	[Person
+		firstName="Logan"
+		lastName="Murray"
+		dateOfBirth=1981-09-24
+		
+		[ChildNode attr=value]
+		[ChildNode attr=value
+			[SubNode attr=value]
+			[AnotherNode]
+		]
+	]
+
+##Why?
+Languages like XML and JSON are immensely useful, but they aren't without their limitations. Gravl aims to solve a few of the biggest problems with these two languages in particular, while keeping its syntax as simple to learn and use as possible.
+
+##The Basics
+Gravl uses a square bracket syntax to represent nodes. Like XML (and unlike JSON), nodes in Gravl are *named* and can recursively contain child nodes. Like JSON (and unlike XML), attributes in Gravl are also fully recursive and can be assigned node values themselves. (Unlike JSON, however, Gravl has no explicit support for arrays, but this is mainly because as you'll see, it doesn't need it.)
+
+Gravl uses only 5 reserved characters: `[`, `]`, `"`, `=`, and `#`. Everything else is Unicode, and considered a valid symbol character.
+
+Gravl also supports a few things that neither XML nor JSON do. For one, unlike XML and JSON, Gravl doesn't require that a document have precisely one root node. It does this by wrapping all documents in their own implicit document node, which typically takes the name of the file or path the document was loaded from, or "Document" by default.
+
+This can be handy as a means to define very simple-structured config files or to attach attributes to a file as a whole.
+
+Here's what an example config file might look like in Gravl:
+
+####userconfig.gravl
+	userId=914713
+	lastLogin="2017-01-01 13:59:01"
+	timeoutMs=20000
+	loadImages=true
+	openFiles=[@
+		"file1.txt"
+		"file2.txt"
+	]
+
+Like XML, Gravl supports **text nodes**, however there are a couple key differences to how text nodes are handled in XML. Specifically, while XML will automatically treat any text found outside of an attribute as a text node, in Gravl any piece of text that includes whitespace (or reserved characters) must be wrapped in double quotes:
+
+	"I am a text node."
+
+While not wrapping the above text in quotes is technically still valid (as the string does not make use of any of the five reserved characters), doing so would treat every word as a separate node, equivalent to the following:
+
+	"I"
+	"am"
+	"a"
+	"text"
+	"node."
+
+…which is probably *not* what was intended.
+
+Gravl is an extremely permissive language in terms of allowed characters. This was one of the major limitations of XML that initially inspired the creation of Gravl. Unlike XML, Gravl allows you to use *any* character in a symbol that is not a reserved character or whitespace, and if that's not enough for you, you can wrap *any* text (including node names, attribute names, and values) in quotes to include any characters at all, including whitespace and reserved characters.
+
+##The Structure of a Node
+A node in Gravl represents an object and is always started with a `[` and ended with a `]`. It consists of three parts:
+
+1. The node's name (required)
+2. The node's attributes (optional)
+3. The node's child nodes (optional)
+
+Its structure looks like this:
+
+	`[` `name` `attributes` `child nodes` `]`
+
+Only the name is required, and must be the first thing inside the node, however you can use as much whitespace as you like between the parts of a node.
+
+The name of a node is generally a **symbol**, which is any consecutive sequence of glyphs excluding reserved characters and whitespace, but like any symbol in Gravl, can also be an arbitrary string if wrapped in double-quotes.
+
+###Attributes
+Attributes are specified as an attribute name followed by an `=` character followed by a value. Attribute values can be either symbols, strings, or another node and must always be defined before any child nodes.
+
+Attributes are represented in a form similar to XML:
+
+	[Node attr1="value1" attr2="value2"]
+
+Unlike XML and JSON, however, attribute values do not need to be wrapped in quotes unless they contain whitespace or reserved characters. The following is valid Gravl:
+
+	[MyNode name=Bob age=24]
+
+Note: Be careful to avoid accidentally using commas! The comma is *not* a reserved character in Gravl and is therefore considered a valid symbol character. If you wrote the following, the attribute's value would actually be "Bob,":
+
+	[MyNode name=Bob, age=24]
+
+Attributes can also recursively represent other nodes. Node-valued attributes are represented exactly the same as standalone nodes: no escaping, wrapping in quotes, or other quirks are needed:
+
+	[MyNode attr1="value" attr2=[AnotherNode]]
+
+Remember, any symbol can be represented as a string in Gravl, even node and attribute names. This is also completely valid:
+
+	["Node name with spaces" "attribute name" = "attribute value"]
+
+However it is recommended you avoid using strings for anything other than values, but Gravl is designed to be very general-purpose and therefore leaves that up to each particular implementation.
+
+###Escaping
+There are only two escaped characters in Gravl: backslash (\) and double-quote ("). Beyond this, strings are expected to contain the literal character you wish to encode.
+
+Like most languages, characters are escaped with the backslash in Gravl:
+
+	[Node attr="Use a \\ to \"escape\" characters in a string."]
+
+###Child Nodes
+A node in Gravl is fully recursive and can contain an ordered set of other child nodes. Everything after the final attribute of a node (if any) are considered the child nodes of that node.
+
+	[Node attr=value
+		[Child
+			[Grandchild]
+		]
+	]
+
+###Arrays
+You may be surprised to learn that like unlike JSON, Gravl doesn't explicitly have a native syntax for arrays. The reason for this is that the Gravl syntax already implicitly supports arrays in the form of child nodes. For example, if we adopt the convention of using the name "@" to represent an array, we can wrap any ordered set of nodes in an array node like so:
+
+	[Node
+		items=[@				# this is actually just a node named @
+			"first value"		# and these are its child nodes
+			"second value"
+			"third value"
+		]
+	]
+
+We can pretend that this is the "official" syntax for arrays, but the truth is it doesn't rely on any new concepts and so is not technically part of the language spec. It's therefore up to each implementation to decide if or how arrays are to be represented, but you can consider this one suggested way.
+
+While it may seem a bit weird at first, there is actually a lot of beauty in this approach. For one thing, it reuses all existing concepts, and is just as expressive as had there been an explicit syntax for arrays: arrays can be hierarchical, and they can contain any combination of other nodes and string values (because ultimately everything—even strings—are nodes in Gravl). As a bonus, because arrays are really just nodes, they can even have their own attributes! While you may argue the usefulness of this, it does demonstrate the power of reusing concepts and keeping the model simple.
+
+It also means we can take full advantage of all the syntactic shorthands in Gravl when working with arrays, that being that symbols (chains of glyphs that do not include whitespace or reserved characters) do not need to be wrapped in quotes. So if the elements in our array don't need such characters, such as numbers, we can very elegantly and concisely represent them in Gravl:
+
+	[Publication
+		years=[@ 1998 2001 2009 2016]
+	]
+
+Doesn't that look nice and clean? Not bad considering we got it all for free.
+
+###Comments
+Comments in Gravl are indicated with the `#` character. Any text following (and including) a `#` up until the end of the line will be skipped over by the parser. You can put comments anywhere, but they always go to the end of the line. There are no inline comments in Gravl.
+
+	# opening comment
+	[Node					# this is a comment
+		attr=value		# this is another comment
+	]
+	# closing comment
+
+##Other Languages
+The first Gravl parser is written in Swift, but as Gravl was designed to be a very general-purpose syntax, I also plan to add implementations in other languages, starting with JavaScript and C#.
+
+If you'd like to contribute by implementing a Gravl parser in your own favourite language, I'd be happy to include it in the project as well. Gravl is really quite a simple syntax, and writing a parser for it can actually be a lot of fun and a great exercise!
+
+I also welcome any feedback and suggestions concerning Gravl, and hope that others out there might find it useful too!
+
+Happy coding!
+
+// devios1
